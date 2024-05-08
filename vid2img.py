8014cde50f1a9e0ec13ab58064731ffa781cab08
BAN Sothen <ban.sothen2016@rupp.edu.kh>
2024-05-08 17:28:13 +0700

    added emoji

--- vid2img.py
+++ vid2img.py
@@ -21,7 +21,7 @@ def capture_images(video_path, num_images, output_quality='1'):
         time_point = interval * (i + 1)
         subprocess.run(['ffmpeg', '-i', video_path, '-ss', str(time_point), '-vframes', '1', '-q:v', output_quality, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    print(f"{num_images} images captured and saved in '{output_dir}' directory.")
+    print(f"✨🎉 {num_images} images captured and saved in {output_dir} directory.")
 
 def get_video_duration(video_path):
     result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', video_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -31,7 +31,7 @@ def get_video_duration(video_path):
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 3:
-        print("Usage: python3 vid2img.py <video_path> <num_images>")
+        print("🤔 Usage: python3 vid2img.py <video_path> <num_images>")
         sys.exit(1)
 
     video_path = sys.argv[1]
